e770915226f9368699494bb7a632a33762043854
Edgar <edgar.a.rivera08@gmail.com>
2020-07-13 10:31:36 -0400

    Created a destroy action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -13,6 +13,11 @@ class Api::V1::ItemsController < ApplicationController
         item = Item.create!(item_params(:category, :user_id))
         render json: item
     end
+    
+    def destroy
+        item = Item.find(params[:id])
+        item.destroy
+    end
 
     private
     def item_params(*args)
