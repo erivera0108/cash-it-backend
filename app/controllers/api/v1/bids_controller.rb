d9fa216adae60839a094188baf8bc451636e040c
Edgar <edgar.a.rivera08@gmail.com>
2020-07-14 11:55:06 -0400

    Added the create action to the bids controller

--- app/controllers/api/v1/bids_controller.rb
+++ app/controllers/api/v1/bids_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::BidsController < ApplicationController
         bids = Bid.all
         render json: bids
     end
+
+    def create
+        bid = Bid.create!(bid_params(:item_id, :user_id, :offer))
+        render json: bid
+    end
+
+    private
+    def bid_params(*args)
+        params.require(:bid).permit(*args)
+    end
 end
